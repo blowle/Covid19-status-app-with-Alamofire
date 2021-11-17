c70f780a4bb469248959c88ab531ab1026a757f3
Yongcheol Lee <dydcjf8889@gmail.com>
2021-11-18 02:01:25 +0900

    add detailVIew.

---
+++ COVID19_KOR_APP/CovidDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19_KOR_APP
+//
+//  Created by YONGCHEOL LEE on 2021/11/18.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
