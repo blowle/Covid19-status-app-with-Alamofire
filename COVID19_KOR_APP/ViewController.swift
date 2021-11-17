eeda9da5f157ff9f5a20dbd717331ac7242a1bd4
Yongcheol Lee <dydcjf8889@gmail.com>
2021-11-18 01:00:49 +0900

    Initial Commit

---
+++ COVID19_KOR_APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19_KOR_APP
+//
+//  Created by YONGCHEOL LEE on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
